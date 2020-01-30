9d5b92e3df1361a0849a93858c6ab0567ed9ea45
Raquel Parra Salom <raquelparrasalom@gmail.com>
2020-01-30 17:53:19 +0100

    second commit

--- src/main.py
+++ src/main.py
@@ -1,19 +1,33 @@
 from flask import Flask
-from flask import request
+from flask import request, redirect, url_for
 from flask import make_response
 from flask import redirect
+from random import randint, uniform,random
 from flask import render_template
+from flask import session
 
 
 
 app =  Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def index():
-    
-    return render_template('index.html')
-   
 
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    return render_template("sign_up.html")
+
+@app.route('/juego', methods=['GET', 'POST'])
+def play():
+    nombre = request.form['name']
+    
+   
+    print(nombre)
+    return render_template("play.html", nombre=nombre, )
+    
 
 if __name__ == '__main__' :
     app.run('0.0.0.0', 5000,debug=True)
